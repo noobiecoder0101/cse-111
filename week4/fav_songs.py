1f98260bd07c50ccd38e1f4d707e725385d56a3b
noobiecoder0101 <edeobauwagbai@gmail.com>
2025-08-04 16:51:35 +0000

    sui

---
+++ week4/fav_songs.py
@@ -0,0 +1,30 @@
+# fav_songs = ["daylight", "billie jean", "the rain"]
+# fav_songs.append("from the start")
+# del fav_songs[0]
+# for songs in fav_songs:
+#     print(f"now playing: {songs}")
+
+#dictionaries study
+
+elements = {
+    "hydrogen": {
+        "molarmass": 1.008,
+        "symbol": "H"
+    },
+    "helium": {
+        "molarmass": 4.0026,
+        "symbol": "He"
+    },
+    "carbon": {
+        "molarmass": 12.011,
+        "symbol": "C"
+    },
+    "oxygen": {
+        "molarmass": 15.999,
+        "symbol": "O"
+    },
+    "nitrogen": {
+        "molarmass": 14.007,
+        "symbol": "N"
+    }
+}
